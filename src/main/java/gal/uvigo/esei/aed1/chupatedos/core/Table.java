71c54677466587ef3fdbdb812302ea848db25d9e
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-03-24 11:49:29 +0100

    Update Table.java

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
@@ -1,13 +1,37 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
-
-
+import java.util.Stack;
 
 public class Table {
 
+    private Stack<Card>[] tableCard;
 
-
+    public Table(Stack<Card>[] tableCard) {
+        this.tableCard = tableCard;
+    }
     
-
-
+    /**
+     * Añade una carta a cada fila de la mesa
+     *
+     * @param c
+     */
+    public void FirstCard(Card[] c) {
+    }
     
+    /**
+     * Coloca la carta en la fila asignada
+     *
+     * @param c
+     */
+    public void PutCard(Card c) {
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cartas en la Mesa: ");
+        sb.append(tableCard);
+        sb.append("\n");
+        return sb.toString();
+    }
+
 }
