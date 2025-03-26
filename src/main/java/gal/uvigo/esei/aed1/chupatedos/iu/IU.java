bac9cc722ec6a08a22a2cb5277950e20d4d0d64a
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-03-26 09:28:38 +0100

    Update IU.java

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/iu/IU.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/iu/IU.java
@@ -63,6 +63,100 @@ public class IU {
         System.out.println(msg);
     }
 
+    /**
+     * Solicita los nombres de los jugadores por teclado y los almacena en una
+     * estructura de datos
+     *
+     * @return Los datos de los jugadores almacenados en la estructura de datos
+     * correspondiente
+     */
+    private int numberPlayers() {
+        int numPlayers = 0;
+        do {
+            numPlayers = readNumber("Insert number of players: ");
+        } while (numPlayers < 2 || numPlayers > 5);
+        return numPlayers;
+    }
+
+    public List<String> askPlayersName() {
+        List<String> name = new ArrayList();
+        int numPlayers = numberPlayers();
+        for (int i = 0; i < numPlayers; i++) {
+            String Name = readString("Insert name of the player: ");
+            name.add(Name);
+        }
+        return name;
+    }
+
+     /**
+     * Muestra por pantalla los datos de un jugador
+     *
+     * @param player Jugador para el cual se mostrarán los datos por pantalla
+     */
+    public void showPlayer(Player player) {
+        System.out.println(player.toString());
+    }
+    
+    /**
+     * Muestra por pantalla los datos de una coleccion de jugadores
+     *
+     * @param players Jugadores cuyos datos se mostrarán por pantalla
+     */
+    public void showPlayers(List<Player> players) {
+        for (Player temp : players) {
+            showPlayer(temp);
+        }
+    }
+    
+    /**
+     * Pide el número de carta que el jugador desea echar
+     * @param player
+     * @return numero de posicion de la carta que se desea jugar
+     */
+    public int pedirNumCarta(Player player) {
+        int num = 0;
+        do {
+            showPlayer(player);
+            num = readNumber("Insert " + player.getName() + " the number of the card selected: ");
+        } while (num < 0 || num > player.getHand().size());
+        return num;
+    }
+    
+    /**
+     * Muestra las cartas seleccionadas por los jugadores
+     * @param list 
+     */
+    public void showSelectedCards(List<Card> list) {
+        System.out.println(list);
+
+    }
+    /**
+     * Muestra las cartas que hay en la mesa
+     * @param table 
+     */
+    public void mostrarMesa(Table table) {
+        System.out.println(table);
+    }
+
+    /**
+     * muestra la carta seleccionada por un jugador
+     * @param player 
+     */
+    public void showSelectedCard(Player player) {
+        System.out.println(player.getName() + " Your card selected is: " + player.getHiddenCard());
+    }
+    
+    /**
+     * Muestra de que jugador es el turno
+     * @param name 
+     */
+
+    public void showPlayerTurn(String name) {
+        System.out.println("The turn is: " + name);
+    }
+
+    
+
 
     
 
