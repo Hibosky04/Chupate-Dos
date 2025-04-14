0db3fbb3f2fa6891ce2cbdf9e45347c20ef3930f
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-04-14 21:00:10 +0200

    Update Game.java

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -1,28 +1,66 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
 import gal.uvigo.esei.aed1.chupatedos.iu.IU;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Game {
+
     private final IU iu;
-    
+    private DeckOfCards deckOfCard;
+    private Table table;
+    private List<Player> players;
+
     public Game(IU iu) {
         this.iu = iu;
+        this.deckOfCard = new DeckOfCards();
+        this.players = new ArrayList<>();
+        this.table = new Table();
 
-  }
-  
-   /**
-   * Metodo principal para jugar
-   */
-  public void play() {
+    }
 
-    
+    /**
+     * Metodo principal para jugar
+     */
+    public void play() {
+        createPlayer();
+        deckOfCard.shuffleDeck();
+        collectCard();
+        table.addPlayedCard(firstCard());
+        iu.showPlayers(players);
+        iu.showTable(table);
+        
+    }
 
+    /**
+     * Crea los jugadores
+     */
+    public void createPlayer() {
+        for (String s : iu.askPlayersName()) {
+            this.players.add(new Player(s));
+        }
+    }
 
+    /**
+     * Reparte diez cartas a cada jugador al inicio de cada ronda
+     */
+    public void collectCard() {
+        for (Player player : players) {
+            while (player.getHand().size() < 7) {
+                player.collectCard(deckOfCard.removeCard());
+            }
 
+        }
+    }
 
-  }
-
- 
+    /** 
+     * @return la carta de la mesa
+     */
+    public Card firstCard() {
+        return this.deckOfCard.removeCard();
+    }
 
 }
+
