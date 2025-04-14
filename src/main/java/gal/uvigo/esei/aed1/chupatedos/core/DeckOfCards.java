446c345a19073d001fcd28f22f33e9e6d41255e1
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-04-14 20:57:40 +0200

    Update DeckOfCards.java

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/DeckOfCards.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/DeckOfCards.java
@@ -1,9 +1,55 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
-
+import java.util.Collections;
+import java.util.Stack;
 
 public class DeckOfCards {
 
+    private Stack<Card> deck;
+    private static final int NumOfCards = 40;
 
+
+    public DeckOfCards() {
+        this.deck = new Stack<>();
+         for(Card cards : Card.values())
+        {
+            deck.push(cards);        
+        }
+    }
+
+    /**
+     * baraja con la instrucción shuffle()
+     */
+    public void shuffleDeck() {
+        Collections.shuffle(this.deck);
+    }
+
+    /**
+     * Quitamos una carta de la baraja
+     *
+     * @return una carta de la baraja
+     */
+    public Card removeCard() {
+        Card toret = deck.pop();
+        return toret;
+    }
+
+    /**
+     * Introducimos una carat en la baraja
+     *
+     * @param c
+     */
+    public void addCard(Card c) {
+        deck.add(c);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Deck of card: ").append(deck);
+        return sb.toString();
+    }
+    
+    
 
 }
