e63edc5bcd10f868d2e8fbd184888449d23ca0c8
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-04-25 14:59:05 +0200

    Update Player.java

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
@@ -4,50 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+
     private String name;
     private List<Card> hand;
-    
+    private Card card;
 
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
-      
+        this.card = null;
+
     }
 
     public String getName() {
         return name;
     }
-    
-    public List<Card> getHand(){
-        return this.hand;
-    }
 
+//    public List<Card> getHand() {
+//        return this.hand;
+//    }
+
+    public Card getCard() {
+        return card;
+    }
+    
+    
+    public void setCard(Card card) {
+        this.card = card;
+    }
     /*
     * el jugador escoge un carta de su mano y la juega
     * @param i
-    */
-    public Card playCard(int i){
+     */
+    public Card playCard(int i) {
         return hand.remove(i);
     }
 
     /**
-     * El jugador recoge la carta que le reparten y la ordena en su mano
-     * (robo de la mano inicial)
-     * @param collectedCard 
+     * El jugador recoge la carta que le reparten (robo
+     * de la mano inicial)
+     *
+     * @param card
      */
-    public void collectCard(Card collectedCard) {
-            hand.add(collectedCard);
+    public void collectCard(Card card) {
+        this.hand.add(card);
     }
-    
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(name);
-        sb.append("\nHand:").append(hand);
+        sb.append("\nHand:").append(this.hand);
         sb.append('\n');
         return sb.toString();
     }
 
-    
 }
