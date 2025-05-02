2561670e3310b98aeadab4927aafa1da4d0b21de
Pablo Bastero Ramos <pablobasram@gmail.com>
2025-05-02 19:20:25 +0200

    Update Game.java
    Se comprueba la carta jugada y si es un 2 (placeholder) y si es un 7 se invierten los turnos

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -3,8 +3,10 @@ package gal.uvigo.esei.aed1.chupatedos.core;
 import gal.uvigo.esei.aed1.chupatedos.iu.IU;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
+import java.util.LinkedList;
 
 public class Game {
 
@@ -17,7 +19,7 @@ public class Game {
     public Game(IU iu) {
         this.iu = iu;
         this.deckOfCard = new DeckOfCards();
-        this.players = new ArrayList<>();
+        this.players = new LinkedList<>();
         this.table = new Table();
 
     }
@@ -30,32 +32,33 @@ public class Game {
         deckOfCard.shuffleDeck();
         collectCard();
         table.addPlayedCard(firstCard());
+        checkCard();
         Player playerTurn = players.get(0);
         boolean endGame = false;
         do {
             iu.showTable(table);
             iu.showPlayerTurn(playerTurn);
-            if (!playerTurn.legalCards(table.upsideCard()).isEmpty()) {
+            if (!playerTurn.legalCards(table.UpsideCard()).isEmpty()) {
                 iu.showPlayer(playerTurn);
-                iu.showLegalCards(playerTurn.legalCards(table.upsideCard()));
-                this.selectCard(playerTurn, playerTurn.legalCards(table.upsideCard()));
+                iu.showLegalCards(playerTurn.legalCards(table.UpsideCard()));
+                this.selectCard(playerTurn, playerTurn.legalCards(table.UpsideCard()));
+                checkCard();
                 endGame = this.endOfGame(playerTurn);
-                playerTurn = this.nextPlayer(playerTurn);
             } else {
                 iu.showAlertNoLegalCards(playerTurn);
                 playerTurn.collectCard(this.loadCard());
-                if (!playerTurn.legalCards(table.upsideCard()).isEmpty()) {
+                if (!playerTurn.legalCards(table.UpsideCard()).isEmpty()) {
                     iu.showPlayer(playerTurn);
-                    iu.showLegalCards(playerTurn.legalCards(table.upsideCard()));
-                    this.selectCard(playerTurn, playerTurn.legalCards(table.upsideCard()));
-                    endGame = this.endOfGame(playerTurn);
-                    playerTurn = this.nextPlayer(playerTurn);
+                    iu.showLegalCards(playerTurn.legalCards(table.UpsideCard()));
+                    this.selectCard(playerTurn, playerTurn.legalCards(table.UpsideCard()));
+                    checkCard();
                 }
                 else{
                     iu.showAlertLostTurn(playerTurn);
-                    playerTurn = this.nextPlayer(playerTurn);
                 }
             }
+            
+            playerTurn=nextPlayer(playerTurn);
         }while(!endGame); 
     }
    
@@ -98,8 +101,7 @@ public class Game {
     public void selectCard(Player player, List<Card> listOfCards) {
         int numCard = iu.askNumCard(listOfCards);
         Card cardSelected = listOfCards.get(numCard);
-        player.playCard(cardSelected);
-        table.addPlayedCard(cardSelected);
+        table.addPlayedCard(player.playCard(cardSelected));
         iu.showSelectedCard(player, cardSelected);
     }
     /*
@@ -107,11 +109,11 @@ public class Game {
      */
     public Card loadCard() {
         if (deckOfCard.getSize() == 0) {
-            Stack<Card> stack = new Stack<>();
-            stack.addAll(table.playedCardsMinusTopPlayed());
-            while (!stack.isEmpty()) {
-                deckOfCard.addCard(stack.pop());
+            Card lastCardPlayed = table.UpsideCard();
+            while (!table.getPlayedCards().isEmpty()) {
+                deckOfCard.addCard(table.removeCard());
             }
+            table.addPlayedCard(lastCardPlayed);
             deckOfCard.shuffleDeck();
         }
         return deckOfCard.removeCard();
@@ -131,5 +133,43 @@ public class Game {
         }
         return false;
     }
+      /*
+    *comprueba si se jugó una carta especial.
+    */
+    public void checkCard(){
+       if(table.checkSpecial()==2){
+           System.out.println("Chupa2"); 
+       }
+       if(table.checkSpecial()==7){
+           players = reverseTurns(players);
+       }
+       
+    }
+    /*
+    * invierte el orden de los turnos
+    */
+    public List<Player> reverseTurns(List<Player> players){
+//        List<Player> temp = new LinkedList<>();
+//        int siz = players.size();
+//        for(int i = 0; i<siz;i++){
+//            temp.add(i,players.get(players.size()));
+//            players.remove(players.size());
+//        }
+//        for(int i = 0; i<siz;i++){
+//         players.add(i,temp.remove(i));
+//        }
+            Stack<Player> temp = new Stack<>();
+            for(int i=0;i<players.size();i++){
+                temp.push(players.get(i));
+            }
+            players.clear();
+            while(!temp.isEmpty()){
+                players.addLast(temp.pop());
+            }
+           
+            return players ;
+ //             Collections.reverse(players);
+    }  
+}
     
-}   
+
