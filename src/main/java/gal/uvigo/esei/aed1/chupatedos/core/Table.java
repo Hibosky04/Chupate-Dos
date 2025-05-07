66fa8e78c41adaf283e21fb46411d719401872cb
Pablo Bastero Ramos <pablobasram@gmail.com>
2025-05-07 23:15:35 +0200

    Update Table.java
    añadidos los arreglos de entrega2

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
@@ -1,31 +1,41 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
-import es.uvigo.esei.aed1.tads.stack.LinkedStack;
-import es.uvigo.esei.aed1.tads.stack.Stack;
+
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Stack;
 
 public class Table {
-    
+
     private Stack<Card> playedCards;
-    
+
     public Table() {
-        this.playedCards = new LinkedStack<>();
+        this.playedCards = new Stack<>();
     }
 
-    public Stack<Card> getPlayedCards() {
-        return playedCards;
+    /**
+     * Devuelve una lista de las cartas jugadas menos la que esta en juego
+     * @return 
+     */
+    public List<Card> playedCardMinusTopPlayed(){
+        List<Card> list = new ArrayList<>();
+        Card upsideCard = playedCards.firstElement();
+        while(!playedCards.isEmpty()){
+            list.addLast(removeCard());
+        }
+        playedCards.add(upsideCard);
+        return list;
     }
+
     /**
     * Agrega una carta a la mesa
     * 
     */
-    
+
     public void addPlayedCard(Card card){
         this.playedCards.push(card);
     }
-    
+
     /**
     * Cantidad de cartas jugadas
     * @return devuelve cuantas cartas ya han sido jugadas
@@ -37,15 +47,15 @@ public class Table {
     *  Carta en juego
     * @return muestra la primera carta de la pila
     */
-    
-    public Card UpsideCard(){
-        return playedCards.top();
+
+    public Card upsideCard(){
+        return playedCards.peek();
     }
-    
+
     /**
      * Elimina una carta de la pila de las ya jugadas
      */
-    
+
     public Card removeCard(){
         return playedCards.pop();
     }
@@ -54,13 +64,13 @@ public class Table {
      * @return numero de la carta en top de table
      */
     public int checkSpecial(){
-        return UpsideCard().getNumber();
+        return upsideCard().getNumber();
     }
         
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("\nUpside Card: ").append(UpsideCard());
+        sb.append("\nUpside Card: ").append(upsideCard());
         sb.append("\nTotal cards played: ").append(size());
         return sb.toString();
     }
