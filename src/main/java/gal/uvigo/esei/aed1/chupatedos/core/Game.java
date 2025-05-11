7a4390366bd6b1214e20a7f2c18c225993b9b2db
Hiba El Ghali El Ghali <164021440+Hibosky04@users.noreply.github.com>
2025-05-11 18:28:12 +0200

    Update Game.java
    el error da en drawTwo
    intente cambiarlo pero no fui capaz

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -1,9 +1,13 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
+import es.uvigo.esei.aed1.tads.stack.LinkedStack;
+import es.uvigo.esei.aed1.tads.stack.Stack;
 import gal.uvigo.esei.aed1.chupatedos.iu.IU;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
-import java.util.LinkedList;
+import java.util.Queue;
 
 public class Game {
 
@@ -16,7 +20,7 @@ public class Game {
     public Game(IU iu) {
         this.iu = iu;
         this.deckOfCard = new DeckOfCards();
-        this.players = new LinkedList<>();
+        this.players = new ArrayList<>();
         this.table = new Table();
 
     }
@@ -26,39 +30,39 @@ public class Game {
      */
     public void play() {
         createPlayer();
-        playerTurn=players.get(0);
         deckOfCard.shuffleDeck();
         collectCard();
         table.addPlayedCard(firstCard());
-        checkCard();
+        checkSpecialCard();
+        Player playerTurn = players.get(0);
         boolean endGame = false;
         do {
             iu.showTable(table);
             iu.displayMessage("\nTurn of: " + playerTurn.getName());
-            if (!playerTurn.legalCards(table.upsideCard()).isEmpty()) {
+            if (!playerTurn.legalCards(table.UpsideCard()).isEmpty()) {
                 iu.showPlayer(playerTurn);
-                iu.showLegalCards(playerTurn.legalCards(table.upsideCard()));
-                this.selectCard(playerTurn, playerTurn.legalCards(table.upsideCard()));
-                checkCard();
+                iu.showLegalCards(playerTurn.legalCards(table.UpsideCard()));
+                this.selectCard(playerTurn, playerTurn.legalCards(table.UpsideCard()));
+                checkSpecialCard();
                 endGame = this.endOfGame(playerTurn);
+                playerTurn = this.nextPlayer(playerTurn);
             } else {
-               iu.displayMessage("\n" + playerTurn.getName() + " doesn't have legal cards, must collect card");
+                iu.displayMessage("\n" + playerTurn.getName() + " doesn't have legal cards, must collect card");
                 playerTurn.collectCard(this.loadCard());
-                if (!playerTurn.legalCards(table.upsideCard()).isEmpty()) {
+                if (!playerTurn.legalCards(table.UpsideCard()).isEmpty()) {
                     iu.showPlayer(playerTurn);
-                    iu.showLegalCards(playerTurn.legalCards(table.upsideCard()));
-                    this.selectCard(playerTurn, playerTurn.legalCards(table.upsideCard()));
-                    checkCard();
-                }
-                else{
+                    iu.showLegalCards(playerTurn.legalCards(table.UpsideCard()));
+                    this.selectCard(playerTurn, playerTurn.legalCards(table.UpsideCard()));
+                    checkSpecialCard();
+                    endGame = this.endOfGame(playerTurn);
+                    playerTurn = this.nextPlayer(playerTurn);
+                } else {
                     iu.displayMessage("\n" + playerTurn.getName() + " don't play a card and loses turn");
                 }
             }
-            
-            playerTurn=nextPlayer(playerTurn);
-        }while(!endGame); 
+            playerTurn = this.nextPlayer(playerTurn);
+        } while (!endGame);
     }
-   
 
     /**
      * Crea los jugadores
@@ -67,21 +71,21 @@ public class Game {
         for (String s : iu.askPlayersName()) {
             this.players.add(new Player(s));
         }
-        
+
     }
 
     /**
      * Reparte siete cartas a cada jugador al inicio de cada ronda
      */
     public void collectCard() {
-        for (int i=0; i<7; i++){
+        for (int i = 0; i < 7; i++) {
             for (Player player : players) {
                 player.collectCard(deckOfCard.removeCard());
             }
         }
     }
 
-    /** 
+    /**
      * @return la carta de la mesa
      */
     public Card firstCard() {
@@ -93,7 +97,6 @@ public class Game {
      *
      * @param player
      * @param listOfCards
-     * @return
      */
     public void selectCard(Player player, List<Card> listOfCards) {
         int numCard = iu.askNumCard(listOfCards);
@@ -101,29 +104,82 @@ public class Game {
         table.addPlayedCard(player.playCard(cardSelected));
         iu.displayMessage(player.getName() + " played: " + cardSelected);
     }
-    /*
-    * Se carga una carta del mazo, si no hay cartas se rellena el mazo con las cartas jugadas menos la ultima que se jugo, se barajea el mazo
+
+    /**
+     * Se carga una carta del mazo, si no hay cartas se rellena el mazo con las
+     * cartas jugadas menos la ultima que se jugo, se barajea el mazo
+     *
+     * @return las cartas al mazo
      */
-  public Card loadCard() {
+    public Card loadCard() {
         if (deckOfCard.getSize() == 0) {
-            Stack<Card> stack = new Stack<>();
-            stack.addAll(table.playedCardMinusTopPlayed());
-            while (!stack.isEmpty()) {
-                deckOfCard.addCard(stack.pop());
+            Card lastCardPlayed = table.UpsideCard();
+            while (!table.getPlayedCards().isEmpty()) {
+                deckOfCard.addCard(table.removeCard());
             }
+            table.addPlayedCard(lastCardPlayed);
             deckOfCard.shuffleDeck();
         }
         return deckOfCard.removeCard();
     }
-  
-     public Player nextPlayer(Player player) {
-        if (player.equals(players.getLast())) {
+
+    /**
+     *
+     * @param player
+     * @return el player sigiente 
+     */
+    public Player nextPlayer(Player player) {
+        if (player.equals(players.getLast())||player == null) {
             return players.getFirst();
         }
         return players.get(players.indexOf(player) + 1);
     }
+    /**
+     * 
+     */
+    public void checkSpecialCard(){
+         if (table.UpsideCard().getNumber() == 7) {
+            this.reverseTurns();
+        }
+        if (table.UpsideCard().getNumber() == 2) {
+            this.drawTwo();
+        }
+    }
+/**
+ * 
+ */
+    public void drawTwo() {
+    Player nextPlayer = nextPlayer(playerTurn);
+    
+    // El jugador siguiente roba 2 cartas
+    for(int i = 0; i < 2; i++ ){
+        nextPlayer.collectCard(loadCard());
+    }
+    iu.displayMessage(nextPlayer.getName() + " has to draw 2 cards and loses their turn!");
+    
+    // Pasamos el turno al siguiente jugador (saltando al que perdió el turno)
+    playerTurn = nextPlayer(nextPlayer);
+}
+  
+/**
+ * 
+ */
+    public void reverseTurns() {
+        Stack<Player> temp = new LinkedStack<>();
+        for (int i = 0; i < players.size(); i++) {
+            temp.push(players.get(i));
+        }
+        players.clear();
+        while (!temp.isEmpty()) {
+            players.addLast(temp.pop());
+        }
+    }
 
-
+    /**
+     * @param player
+     * @return verdadero si el jugador tiene la mano vacia y se acaba el juego y
+     * si fuera falso que siga el juego
+     */
     public boolean endOfGame(Player player) {
         if (player.checkHand()) {
             iu.displayMessage("\nThe winner is: " + player.getName());
@@ -131,41 +187,4 @@ public class Game {
         }
         return false;
     }
-    
-public void checkCard(){
-       if(table.checkSpecial()==2){
-           drawTwo();
-           iu.displayMessage("Player " + playerTurn.getName()+" draws two and losses turn");
-
-       }
-       if(table.checkSpecial()==7){
-           reverseTurns();
-           iu.displayMessage("Turns have been inverted");
-       }
-       
-    }
-    /*
-    * se avanza el turno y el jugador roba 2 cartas
-    */
-    public void drawTwo(){
-        playerTurn=nextPlayer(playerTurn);
-        for(int i = 0; i<2;i++){
-        playerTurn.collectCard(this.loadCard());
-        }
-    }
-           
-    /*
-    * invierte el orden de los turnos
-    */
-    public void reverseTurns(){
-            Stack<Player> temp = new Stack<>();
-            for(int i=0;i<players.size();i++){
-                temp.push(players.get(i));
-            }
-            players.clear();
-            while(!temp.isEmpty()){
-                players.addLast(temp.pop());
-            }
-            
-    }  
 }
